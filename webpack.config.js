e8b29b808e02481713e3389ce04404da31254391
fix777 <wangguoguo.lan@gmail.com>
2017-07-27 15:41:53 +0800

    remove useless code

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+// // Another workaround
 // const tsc = require("typescript");
 
 // const webpackConfig = require("fs").readFileSync("./webpack.config.ts", "utf8");
@@ -13,29 +14,3 @@
 // };
 
 // eval(tsc.transpileModule(webpackConfig, options).outputText);
-
-// const path = require("path");
-
-// module.exports = (env) => {
-//   console.log(env);
-
-//   return {
-//     entry: [
-//       path.resolve(__dirname, "site", "index"),
-//     ],
-//     resolve: {
-//       extensions: [".js", ".jsx", ".css"]
-//     },
-//     module: {
-//       rules: [
-//         { test: /\.jsx?$/, use: ["babel-loader"], exclude: /node_modules/ },
-//         { test: /\.css$/, use: ["style-loader", "css-loader"] },
-//       ]
-//     },
-//     output: {
-//       path: path.join(__dirname, "/_site"),
-//       publicPath: "/",
-//       filename: "bundle.js"
-//     },
-//   };
-// }
